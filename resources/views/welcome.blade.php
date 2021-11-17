63b239a5a3d9f41d8baa01237cc1036d4404fddb
Sharon Excelli Lauder <sharonexcellilauder@gmail.com>
2021-11-17 15:06:27 +0700

    webprofile

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+
+
+        <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>Setup Laravel Bootstrap Successfully</h1>
+        <i class="fas fa-address-card"></i>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
