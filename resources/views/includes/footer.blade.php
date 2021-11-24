32e4bb4eab264f1a405c73f6df4eabd4e2d41c96
Sharon Excelli Lauder <sharonexcellilauder@gmail.com>
2021-11-24 14:33:40 +0700

    webprofile

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; 2021 Sharon Excelli. All Rights Reserved.</small>
+    </div>
+</footer>
+<script src="{{ mix('js/app.js') }}"></script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -20,4 +20,22 @@
         <i class="fas fa-address-card"></i>
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
+</html> --}}
+
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('pages.header')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
 </html>
